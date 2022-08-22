75f069cc14edc1a4a6e5f6b1b7d7fa9531c9c23c
zhengcunzhe <2462591288@qq.com>
2022-08-22 21:23:41 +0800

    dubbo

---
+++ src/test/java/com/zcz/dubbo/DubboApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zcz.dubbo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DubboApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
